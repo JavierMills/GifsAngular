2918ef1edb6220dbe5510f9399211f02cfe600e0
javiermills <javier_bouncing@hotmail.com>
2022-06-10 13:49:53 -0500

    addGifs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShareModule } from './share/share.module';
+import { GiftsModule } from './gifts/gifts.module';
+import { HttpClientModule } from '@angular/common/http'
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
-  imports: [
-    BrowserModule
+  imports: [  
+    BrowserModule,
+    ShareModule,
+    GiftsModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
